2a0f4d7c2d2f91bb434d8147badb7bfb803b4b72
Seneca Manu <senecamanu.a@gmail.com>
2018-08-07 16:51:14 +0700

    tambahan 2, august 7

--- Phase 0/Week2/2-2-3-ifElseTry2.js
+++ Phase 0/Week2/2-2-3-ifElseTry2.js
@@ -1,33 +1,30 @@
 //FLEXIBLE CODE
-var name = 'mifzan';
+var name = '';
 var role = '';
 
 if (name == '' && role == '') {
   console.log("Nama dan peran harus diisi!");
 }
 
-if (name.length > 1 && role == '') {
-  console.log("peran harus diisi!");
+else if (name.length > 1 && role == '') {
+  console.log('Halo ' + name + ', pilih peranmu untuk memulai game!');
 }
 
-else if (role == '') {
-  console.log("Halo " + name + ", Pilih peranmu untuk memulai game!");
-  console.log("Halo Penyihir Zero, ciptakan keajaiban yang membantu kemenanganmu!");
+else if (name == '' && role.length > 1) {
+  console.log("nama harus diisi!");
 }
 
-else if (role == 'Ksatria') {
-  console.log("Selamat datang di Dunia Proxytia, " + name + ".");
-  console.log("Halo Ksatria " + name + ", kamu dapat menyerang dengan senjatamu!");
+else if (name.length > 1 && role == 'Ksatria' ) {
+  console.log('Selamat datang di Dunia Proxytia, ' + name + '.');
+  console.log('Halo ' + role + ' ' + name + ', kamu dapat menyerang dengan senjatamu!');
 }
-
-else if (role == 'Tabib') {
-  console.log("Selamat datang di Dunia Proxytia, "+ name +".");
-  console.log("Halo Tabib " + name + ", kamu akan membantu temanmu yang terluka.");
+else if (name.length > 1 && role == 'Tabib' ) {
+  console.log('Selamat datang di Dunia Proxytia, ' + name + '.');
+  console.log('Halo ' + role + ' ' + name + ', kamu akan membantu temanmu yang terluka.');
 }
-
-else if (role == 'Penyihir') {
-  console.log("Selamat datang di Dunia Proxytia, "+ name +".");
-  console.log("Halo Penyihir " + name + ", ciptakan keajaiban yang membantu kemenanganmu!");
+else if (name.length > 1 && role == 'Penyihir' ) {
+  console.log('Selamat datang di Dunia Proxytia, ' + name + '.');
+  console.log('Halo ' + role + ' ' + name + ', ciptakan keajaiban yang membantu kemenanganmu!');
 }
 
 else {
